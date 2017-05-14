aee044a6f698b811c85e6ddf61212ba739ba888e
mayela <mayela0x14@gmail.com>
2017-05-14 08:38:01 -0500

    Changes on urls. Views for the detail of sale and rent properties

--- properties/views.py
+++ properties/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from properties.models import Address, SaleProperty, RentProperty
 
@@ -8,5 +9,17 @@ class SalePropertyListView(ListView):
     model = SaleProperty
 
 
+class SalePropertyDetailView(DetailView):
+    model = SaleProperty
+    http_method_names = ['get']
+
+
 class RentPropertyListView(ListView):
     model = RentProperty
+
+
+class RentPropertyDetailView(DetailView):
+    model = RentProperty
+    http_method_names = ['get']
+    template_name = 'rent-detail-prperty.html'
+
